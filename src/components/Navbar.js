2e53e2bffaf9165e4fafdb5e19e5742394bf8934
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-04-17 20:24:21 +0530

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,31 @@
-import React from "react";
+import { useState } from "react";
+import Navigation from "./Navigation";
+import Hamburger from "../assets/hamburger.png";
+import Close from "../assets/close.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  function handleToggle() {
+    setNavbarOpen(!navbarOpen);
+  }
+
+  return (
+    <nav>
+      <nav className="burger">
+        <img
+          src={require("../assets/nav-logo.png")}
+          alt="Little Lemon logo"
+          className="nav-image"
+        ></img>
+
+        <button className="burger-icon" onClick={handleToggle}>
+          <img src={navbarOpen ? Close : Hamburger} alt="Navigation Bar" />
+        </button>
+      </nav>
+      <Navigation device="desktop" />
+      {navbarOpen ? <Navigation device="mobile" /> : ""}
+    </nav>
+  );
 };
 
 export default Navbar;
